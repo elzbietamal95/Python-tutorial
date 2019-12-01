77bea997889d3c8a74f5819453dba374f3acbc69
ela <elzbietamalinowska95@gmail.com>
2019-12-01 19:47:51 +0100

    Problem 4 in Midterm Exam Solved. Solution of Problem 2 in Set 1 improved (the earlier solution for some cases had IndexError: string index out of range)

---
+++ Midterm_Exam/MidExam_Problem4.py
@@ -0,0 +1,48 @@
+def is_triangular(k):
+    """
+    k, a positive integer
+    returns True if k is triangular and False if not
+
+    wzor na n-ty wyraz ciagu triangulacji: a_0 = 0, a_(n) = a_(n-1) + n
+    rozwiazanie rekurencyjne tego ciagu to: a_(n) = [n(n+1)]/2
+    """
+    i = 0
+    triangular_number = 0
+    while triangular_number <= k:
+        i += 1
+        triangular_number = (i*(i+1))/2
+        if triangular_number == k:
+            return True
+    return False
+
+def triangular(k):
+    """
+    funkcja wypisujaca k pierwszych elementow ciagu triangulacji
+    np. dla k =7 funkcja wypisze:
+    [1, 3, 6, 10, 15, 21, 28]
+    """
+    triangular_numbers = []
+    number = 0
+    for i in range(1, k + 1):
+        number += i
+        triangular_numbers.append(number)
+    print(triangular_numbers)
+
+def trian(k):
+    """
+    druga wersja funkcji "triangular"
+    """
+
+    num = 0
+    i = 0
+    trian_nums = []
+    while i < k:
+        i += 1
+        num += i
+        trian_nums.append(num)
+    print(trian_nums)
+
+
+triangular(7)
+trian(7)
+print(is_triangular(10))
--- Week1/Week1-Problem2.py
+++ Week1/Week1-Problem2.py
@@ -1,10 +1,10 @@
 s = 'azcbobobegghakl'
 sum_bob = 0
 
-for mark in range(len(s)):
-    if s[mark] == 'b':
-        if s[mark + 1] == 'o':
-            if s[mark + 2] == 'b':
+for i in range(len(s)):
+    if s[i] == 'b' and i != len(s) and i != len(s)-1 and i != len(s)-2:
+        if s[i + 1] == 'o':
+            if s[i + 2] == 'b':
                 sum_bob += 1
 
 print(sum_bob)
