b4edc1eb33cfc05eec438aa40ef38e7260521d3b
ela <elzbietamalinowska95@gmail.com>
2019-12-04 21:49:29 +0100

    Miterm Exam: Solution of Problem 9 corrected

--- Midterm_Exam/MidExam_Problem9.py
+++ Midterm_Exam/MidExam_Problem9.py
@@ -1,10 +1,13 @@
 def permutation(L1, L2):
+    result = False
     if len(L1) == len(L2):
-        for i in range(0, len(L1)):
-            if L1.count(L1[i]) == L2.count(L1[i]):
-                return True
+        for element in L1:
+            if L1.count(element) == L2.count(element):
+                result = True
+            else:
+                return False
+    else:
         return False
-    return False
 
 def is_list_permutation(L1, L2):
     if len(L1) == 0 and len(L2) == 0:
@@ -17,7 +20,6 @@ def is_list_permutation(L1, L2):
         type_most_occur_elem = type(most_occur_elem)
         return (most_occur_elem, how_many_times_occur, type_most_occur_elem)
 
-
 L1 = [1, 'b', 1, 'c', 'c', 1]
 L2 = ['c', 1, 'b', 1, 1, 'c']
 
@@ -27,3 +29,4 @@ L2 = ['c', 1, 'b', 1, 1, 'c']
 #L1 = []
 #L2 = []
 print(is_list_permutation(L1, L2))
+print(is_list_permutation([0, 4, 8, 3, 0, 2, 2, 1, 4, 7, 8, 3, 7, 0, 0], [3, 4, 0, 3, 8, 0, 2, 0, 7, 2, 0, 3, 7, 2, 1]))
